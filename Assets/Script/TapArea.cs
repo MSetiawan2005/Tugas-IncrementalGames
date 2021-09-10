62f2887814bea172045fbf7eab650cbfe3ce99ca
MSetiawan2005 <ms0132005@gmail.com>
2021-09-11 02:07:35 +0700

    init commit

---
+++ Assets/Script/TapArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource buttonSound;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonSound.PlayOneShot(buttonSound.clip);
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
